fb9ed15b6866deb4d4c9e618ecd983b1083ef389
Saint-Jean Germila <germila@noukod.com>
2018-09-26 19:04:41 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,7 +1,10 @@
 collection = [1, 2, 3, 4]
-def my_each
-my_each(collection) do |i|
-  puts i
+def my_each array
+  i = 0
+  while i < = 4
+    array.my_each_collection do |i|
+      put "#{i}"
+    
 end # put argument(s) here
   # code here
 end
